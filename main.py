e06690ed4a499c5e73846951bca26f0185fd5ffb
Nate <inathan.m@gmail.com>
2019-12-06 12:56:45 -0800

    all algos

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
-import matplotlib as plt
 import pandas as pd
 import math
 
 large = "CS170_LARGEtestdata/"
 small = "CS170_SMALLtestdata/"
 
-def leave_one_out_cross_validation(data, current_features, feature_to_add):
-    df = pd.read_csv(data, delimiter= '\s+', header=None)
+def leave_one_out_cross_validation(data, current_features, feature_to_add=-1, feature_to_remove=-1, algo=False, best_accuracy_so_far=0):
+    df = pd.read_csv(data, delimiter= '\s+', index_col=False, header=None)
     correct = 0
-    features = current_features
-    features.append(feature_to_add)
+    features = current_features[:]
+    if feature_to_add > -1:
+        features.append(feature_to_add)
+    if feature_to_remove > -1:
+        features.remove(feature_to_remove)
 
     for index, row in df.iterrows():
         closest_dist = math.inf
@@ -25,48 +27,62 @@ def leave_one_out_cross_validation(data, current_features, feature_to_add):
                     closest_index = index2
         if df.loc[index][0] == df.loc[closest_index][0]:
             correct += 1
+        if algo:
+            if (len(df.index) - index + correct) < best_accuracy_so_far:
+                print("low accuracy, terminating early", correct)
+                return correct 
                 
-    print("accuracy", correct / len(df.index))
-    return correct / len(df.index)
-    #print(df.loc[0][1])
+    print("accuracy", correct)
+    return correct 
 
-def forward_feature_search(data, algo):
-    df = pd.read_csv(data, delimiter= '\s+', index_col=False)
+def forward_feature_search(data, algo=False):
+    df = pd.read_csv(data, delimiter= '\s+', index_col=False, header=None)
     current_features = []
+    best_accuracy_so_far = 0
     for i in range(1,len(df.columns)):
         print("on the "+str(i)+"th level of the search tree")
-        best_accuracy_so_far = accuracy = 0
         feature_to_add = -1
         for k in range(1,len(df.columns)):
             if k not in current_features:
                 print("considering adding "+str(k)+"th feature")
-                accuracy = leave_one_out_cross_validation(data,current_features,k)
-            if accuracy > best_accuracy_so_far:
-                best_accuracy_so_far = accuracy
-                feature_to_add = k
+                if algo:
+                    accuracy = leave_one_out_cross_validation(data,current_features, feature_to_add=k, algo=True, best_accuracy_so_far=best_accuracy_so_far)
+                else:
+                    accuracy = leave_one_out_cross_validation(data,current_features, feature_to_add=k)
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add = k
+        print("best so far: ",best_accuracy_so_far)
         if feature_to_add > -1:
             current_features.append(feature_to_add)
             print("on level "+str(i)+" added feature "+str(feature_to_add)+" to current set")
         else:
-            print("nothing added to current set")
+            print("nothing added to current set", current_features)
+            return
 
-def backward_feature_search(data, algo):
-    df = pd.read_csv(data, delimiter= '\s+', index_col=False)
+    print(current_features)
+
+def backward_feature_search(data):
+    df = pd.read_csv(data, delimiter= '\s+', index_col=False, header=None)
     current_features = list(range(1, len(df.columns)))
+    worst_accuracy_so_far = accuracy = math.inf
     for i in range(1,len(df.columns)):
         print("on the "+str(i)+"th level of the search tree")
-        worst_accuracy_so_far = accuracy = math.inf
         feature_to_remove = -1
         for k in range(1,len(df.columns)):
             if k in current_features:
                 print("considering removing "+str(k)+"th feature")
-                accuracy = leave_one_out_cross_validation(data,current_features,k)
-            if accuracy < worst_accuracy_so_far:
-                worst_accuracy_so_far = accuracy
-                feature_to_remove = k
+                accuracy = leave_one_out_cross_validation(data,current_features,feature_to_remove=k)
+                if accuracy < worst_accuracy_so_far:
+                    worst_accuracy_so_far = accuracy
+                    feature_to_remove = k
+        print("worst so far: ",worst_accuracy_so_far)
         if feature_to_remove > -1:
-            current_features.remove(feature_to_add)
-        print("on level "+str(i)+" removed feature "+str(feature_to_remove)+" from current set")
+            current_features.remove(feature_to_remove)
+            print("on level "+str(i)+" removed feature "+str(feature_to_remove)+" from current set")
+        else:
+            print("nothing removed from current set")
+    print(current_features)
 
 #run the program, collect user input
 def main():
@@ -85,9 +101,11 @@ def main():
         print("error: bad input")
         return
     if algo_choice == "1":
-        forward_feature_search(data_choice, algo_choice)
-    if algo_choice == "2":
-        backward_feature_search(data_choice, algo_choice)
+        forward_feature_search(data_choice)
+    elif algo_choice == "2":
+        backward_feature_search(data_choice)
+    elif algo_choice == "3":
+        forward_feature_search(data_choice, True)
 
 if __name__== "__main__":
     main()
